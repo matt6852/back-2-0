96fa965ffb52ed413a2063db8d2a926589c97bb2
Сергей Мячин <s.myachin@Sergey-MacBook-Air.local>
2022-12-02 12:30:15 +0300

    comment check cookies in get me router

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -38,7 +38,7 @@ authRouter.post(
     if (result) {
       res.cookie("refreshToken", result.refreshToken, {
         httpOnly: true,
-        secure: true,
+        // secure: true,
       });
       return res.status(200).json(result.accessToken);
     }
@@ -53,7 +53,7 @@ authRouter.post(
     const refreshToken = jwtAuth.createRefreshToken(req.user?.id);
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: true,
+      // secure: true,
     });
     return res.status(200).json(accessToken);
   }
@@ -101,7 +101,7 @@ authRouter.post(
 authRouter.get(
   "/me",
   authJWTMiddleware,
-  checkCookies,
+  // checkCookies,
   async (req: Request, res: Response) => {
     const me = {
       email: req?.user?.email,
