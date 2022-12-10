eafc3c38f285412bd1270f2c4a705106584a8ec1
Сергей Мячин <s.myachin@Sergey-MacBook-Air.local>
2022-12-10 14:52:42 +0300

    attempt 1

--- src/repo/devices/query-device-repo.ts
+++ src/repo/devices/query-device-repo.ts
@@ -1,9 +1,24 @@
 import { DeviceModel } from "../../models/deviceModal";
-
+const mappedDevices = (devices: any) => {
+  const result = devices.map((d: any) => {
+    return {
+      deviceId: d.deviceId,
+      lastActiveDate: d.lastActiveDate,
+      ip: d.ip,
+      userId: d.userId,
+      title: d.title,
+    };
+  });
+  return result;
+};
 export const queryDevicesRepo = {
   async getAllDevices() {
     try {
-      return await DeviceModel.find({});
+      const res = await DeviceModel.find({});
+      // const mapped = mappedDevices(res);
+      // console.log(mapped);
+
+      return mappedDevices(res);
     } catch (error) {
       return null;
     }
